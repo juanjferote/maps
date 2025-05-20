e0ea22d5e52cb64b8b46532a76e181c06a80d4b3
juanjferote <juanj.ferote@educa.jcyl.es>
2025-05-20 16:41:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+let map;
+
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.4165, lng: -3.70256 },
     zoom: 8,
     });
+
+document.getElementById("ciudad").addEventListener('change', function() {
+    const ciudad = document.getElementById("ciudad").value
+    var ciudadSeleccionada
+    switch (ciudad) {
+    case "leon":
+        ciudadSeleccionada = { lat: 42.60002, lng: -5.57032}
+        break; 
+    case "dublin":
+        ciudadSeleccionada = { lat: 53.3498, lng: -6.2603}
+        break;
+    case "bogota":
+        ciudadSeleccionada = { lat: 4.7110, lng: -74.0721}
+        break;
+    case "varsovia":
+        ciudadSeleccionada = { lat: 52.2297, lng: 21.0122}
+        break;
+    case "tokio":
+        ciudadSeleccionada = { lat: 35.6895, lng: 139.6917}
+        break;
+    default:
+        ciudadSeleccionada = { lat: 40.4165, lng: -3.70256}
+        break;
+    }
+    map.panTo(ciudadSeleccionada);
+    map.setZoom(12)
+  });
 }
