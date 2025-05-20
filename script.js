2e14af22e00fe71e07ab800b1a24ffe5e2379c9e
juanjferote <juanj.ferote@educa.jcyl.es>
2025-05-20 16:11:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 40.4165, lng: -3.70256 },
     zoom: 8,
     });
 }
